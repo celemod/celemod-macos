7b3a2b036f4e815ba1bcbfb5c0b37e759a8bb9be
2nthony <ant.hu@outlook.com>
2026-07-01 20:11:20 +0800

    move toast to top

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ const AppLayout = () => {
   return (
     <Fragment>
       <AlertProvider />
-      <Toast.Provider />
+      <Toast.Provider placement="top" />
       {/* @ts-ignore */}
       <GlobalContext.Provider
         value={{
